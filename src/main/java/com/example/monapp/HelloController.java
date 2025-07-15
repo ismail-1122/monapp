7aefbecb7f005381a400a7c5b7f864d9d3eb4251
ismail-1122 <ismailkellal25@gmail.com>
2025-07-15 11:43:51 +0100

    Initial commit

---
+++ src/main/java/com/example/monapp/HelloController.java
@@ -0,0 +1,11 @@
+package com.example.monapp;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Bonjour Ismail";
+    }
+}
