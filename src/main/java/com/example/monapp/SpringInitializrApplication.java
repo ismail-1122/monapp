1e8b9a79dc4f78469fd26f982633de7787a0a64e
ismail-1122 <ismailkellal25@gmail.com>
2025-07-22 12:07:44 +0100

    kaniko+ kuberntes

--- src/main/java/com/example/monapp/SpringInitializrApplication.java
+++ src/main/java/com/example/monapp/SpringInitializrApplication.java
@@ -1,13 +1,31 @@
 package com.example.monapp;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringInitializrApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringInitializrApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner init(UserRepository userRepository) {
+        return args -> {
+
+            userRepository.findByEmail("ismail@gmail.com").ifPresentOrElse((user) -> {
+                    },
+                    () -> {
+                        User user = new User();
+                        user.setName("ismail");
+                        user.setEmail("ismail@gmail.com");
+                        userRepository.save(user);
+                    });
+
+        };
     }
 
 }
